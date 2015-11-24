9c983fac3960a9819b8d68822738107704c1510d
Guilherme Moura <guime.sprint@gmail.com>
2015-11-24 19:58:12 -0300

    Added initial project.

---
+++ RFProgressButton/RFProgressButton.h
@@ -0,0 +1,19 @@
+//
+//  RFProgressButton.h
+//  RFProgressButton
+//
+//  Created by Guilherme Moura on 11/24/15.
+//  Copyright © 2015 Reefactor, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RFProgressButton.
+FOUNDATION_EXPORT double RFProgressButtonVersionNumber;
+
+//! Project version string for RFProgressButton.
+FOUNDATION_EXPORT const unsigned char RFProgressButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RFProgressButton/PublicHeader.h>
+
+
